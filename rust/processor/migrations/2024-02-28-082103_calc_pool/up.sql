7c8a4eecc47f4952def8c5b6f5df56fda159550b
vladimirovmm <vladimirov.m.m@mail.ru>
2024-02-28 21:18:57 +0700

    fix: calc_pool

--- rust/processor/migrations/2024-02-28-082103_calc_pool/up.sql
+++ rust/processor/migrations/2024-02-28-082103_calc_pool/up.sql
@@ -1,4 +1,5 @@
 
+
 CREATE OR REPLACE function calc_pool(fpool_id varchar)
 returns int as
 $$
@@ -12,44 +13,54 @@ DECLARE
    	calc_ids varchar[] := array[]::varchar[];
 begin
 	
-	FOR temprow IN
-        SELECT le.id, le.pool_id, le.x_val, le.y_val, le.fee, le.version 
-        	FROM ls_events le
-        	where le.pool_id = fpool_id and le.calc = false and (
-        		(le.x_val is not null and le.x_val != 0 ) 
-        			or (le.y_val is not null and le.y_val != 0 )
-        			or (le.x_val is not null and le.x_val != 0 )
-    		)
-        	order by le.version asc
-    		LIMIT 4000
-    loop
-		cn := cn + 1;
-		last_version := temprow.version;
+	select sum(le.x_val),sum(le.y_val), array_agg(le.id), max(le."version")
+		into nx_val, ny_val, calc_ids, last_version
+		from (
+			select l1.id, l1.x_val, l1.y_val, l1."version" 
+				from ls_events l1 
+				where 
+					l1.pool_id = '92a538bba6b8c1932815e0064e84f35c1c37781a30aebe52e776edff782b4d9b'
+					and l1.calc = false
+				order by l1.version asc
+				limit 100
+		) le;
+	
+	select le.fee into nfee
+		from ls_events le 
+		where le.id = any (calc_ids) and le.fee is not null 
+		order by id desc
+		limit 1;
 		
-		if temprow.fee > 0 then 
-			nfee := temprow.fee;
-		end if;
 	
-		nx_val := nx_val + temprow.x_val;
-		ny_val := ny_val + temprow.y_val;
+	cn := array_length(calc_ids,1);
 
-	
-		calc_ids = calc_ids || array[temprow.id];
-    END LOOP;
-   
-	update ls_events set calc = true where id = any (calc_ids);
-   
-   	if nfee > 0 then
+   	if cn is null then
+   		return 0;
+   	end if;	
+
+	if nfee <> 0 then
    		update ls_pools
    			set fee = nfee, last_tx_version  = last_version
    			where lp.id = fpool_id;
    	end if;
-   
+
    	if nx_val <> 0 or ny_val <> 0 then
    		update ls_pools 
    			set x_val = x_val + nx_val, y_val = y_val + ny_val, last_tx_version = last_version
    			where id = fpool_id;
-   	end if;
+   	end if;	
+
+   	update ls_events 
+		set calc = true 
+		where id = any (calc_ids);
+   
+   	raise info '= = = ';
+   	raise info 'cn: %', cn;
+   	raise info 'nfee: %', nfee;
+    raise info 'nx_val: %', nx_val;
+   	raise info 'nx_val: %', nx_val;
+    raise info 'calc_ids: %', calc_ids;
+    raise info 'last_version: %', last_version;
    
     return cn;
  end;
