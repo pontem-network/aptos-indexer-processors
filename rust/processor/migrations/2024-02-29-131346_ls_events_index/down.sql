692b5a3b40f052c116278d127815646b0ba45e38
vladimirovmm <vladimirov.m.m@mail.ru>
2024-02-29 21:05:04 +0700

    feat: added indexes to ls_events

---
+++ rust/processor/migrations/2024-02-29-131346_ls_events_index/down.sql
@@ -0,0 +1,3 @@
+-- This file should undo anything in `up.sql`
+DROP INDEX ls_events_x_val_idx;
+DROP INDEX ls_events_y_val_idx;
---
+++ rust/processor/migrations/2024-02-29-131346_ls_events_index/up.sql
@@ -0,0 +1,3 @@
+-- migration
+CREATE INDEX ls_events_x_val_idx ON ls_events (x_val);
+CREATE INDEX ls_events_y_val_idx ON ls_events (y_val);
