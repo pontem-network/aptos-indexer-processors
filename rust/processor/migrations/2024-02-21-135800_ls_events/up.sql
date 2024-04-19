85a041ba044722f4882d1f9cf7dbd8dc87c23218
Maxim Vladimirov <32911557+vladimirovmm@users.noreply.github.com>
2024-04-19 14:38:56 +0700

    feat: Getting a "fee", "dao_fee", "x_val" and "y_val" from resources (#5)

--- rust/processor/migrations/2024-02-21-135800_ls_events/up.sql
+++ rust/processor/migrations/2024-02-21-135800_ls_events/up.sql
@@ -7,7 +7,7 @@ CREATE TYPE event_type AS ENUM (
     'oracle_updated_event',
     'update_fee_event',
     'update_dao_fee_event',
-    'coin_deposited_event',
+    'coin_deposited_event'
 );
 
 CREATE TABLE public.ls_events (
