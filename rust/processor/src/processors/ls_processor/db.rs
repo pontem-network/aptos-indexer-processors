8975e87173dc2bb6eb93ef48cd556a34b2c2c281
Maxim Vladimirov <32911557+vladimirovmm@users.noreply.github.com>
2024-04-19 14:38:56 +0700

    feat: Getting a "fee", "dao_fee", "x_val" and "y_val" from resources (#5)

--- rust/processor/src/processors/ls_processor/db.rs
+++ rust/processor/src/processors/ls_processor/db.rs
@@ -1,8 +1,11 @@
-use std::str::FromStr;
+use std::{collections::HashMap, str::FromStr};
 
 use anyhow::{bail, Result};
 use bigdecimal::BigDecimal;
-use diesel::{deserialize::Queryable, Insertable, Selectable};
+use diesel::{
+    deserialize::Queryable, query_dsl::methods::FilterDsl, ExpressionMethods, Insertable,
+    Selectable,
+};
 use diesel_async::RunQueryDsl;
 use tonic::async_trait;
 use tracing::info;
@@ -12,6 +15,8 @@ use crate::{
     utils::database::PgPoolConnection,
 };
 
+use super::mv::PoolType;
+
 // Write 100 values at a time to the table
 const TB_CHUNKS_SIZE: usize = 100;
 
@@ -20,7 +25,7 @@ pub(crate) trait InsertToDb {
     async fn insert_to_db(self, conn: &mut PgPoolConnection<'_>) -> Result<()>;
 }
 
-#[derive(Selectable, Queryable, Insertable, Debug, Clone)]
+#[derive(Selectable, Queryable, AsChangeset, Insertable, Debug, Clone)]
 #[diesel(table_name = ls_pools)]
 pub struct TableLsPool {
     pub id: String,
@@ -32,23 +37,97 @@ pub struct TableLsPool {
     pub y_val: BigDecimal,
     pub fee: i64,
     pub dao_fee: i64,
-    pub last_event: i64,
+    pub last_version: i64,
+}
+
+impl From<&PoolType> for TableLsPool {
+    fn from(pool_type: &PoolType) -> Self {
+        TableLsPool {
+            id: pool_type.hash(),
+            version_ls: "".to_string(),
+            x_name: pool_type.x_name.clone(),
+            y_name: pool_type.y_name.clone(),
+            curve: pool_type.curve.clone(),
+            x_val: BigDecimal::from(0),
+            y_val: BigDecimal::from(0),
+            fee: 0,
+            dao_fee: 0,
+            last_version: 0,
+        }
+    }
+}
+
+impl From<PoolType> for TableLsPool {
+    fn from(pool_type: PoolType) -> Self {
+        TableLsPool {
+            id: pool_type.hash(),
+            version_ls: "".to_string(),
+            x_name: pool_type.x_name,
+            y_name: pool_type.y_name,
+            curve: pool_type.curve,
+            x_val: BigDecimal::from(0),
+            y_val: BigDecimal::from(0),
+            fee: 0,
+            dao_fee: 0,
+            last_version: 0,
+        }
+    }
 }
 
 #[async_trait]
 impl InsertToDb for Vec<TableLsPool> {
-    async fn insert_to_db(self, conn: &mut PgPoolConnection<'_>) -> Result<()> {
-        for rows in self.chunks(TB_CHUNKS_SIZE) {
-            let count = diesel::insert_into(schema::ls_pools::table)
-                .values(rows)
+    async fn insert_to_db(mut self, conn: &mut PgPoolConnection<'_>) -> Result<()> {
+        self.group();
+
+        let mut updated_count = 0;
+        let count = self.len();
+
+        for pool in &self {
+            updated_count += diesel::insert_into(schema::ls_pools::table)
+                .values(pool)
                 .on_conflict(schema::ls_pools::id)
-                .do_nothing()
+                .do_update()
+                .set((
+                    schema::ls_pools::x_val.eq(&pool.x_val),
+                    schema::ls_pools::y_val.eq(&pool.y_val),
+                    schema::ls_pools::fee.eq(&pool.fee),
+                    schema::ls_pools::dao_fee.eq(&pool.dao_fee),
+                    schema::ls_pools::last_version.eq(&pool.last_version),
+                ))
+                .filter(schema::ls_pools::last_version.lt(&pool.last_version))
                 .execute(conn)
                 .await?;
-            info!("{count} TableLsPool added");
         }
+        info!("{count}:{updated_count} TableLsPool added/updated");
 
         Ok(())
+    }
+}
+
+trait GroupPool {
+    fn group(&mut self);
+}
+
+impl GroupPool for Vec<TableLsPool> {
+    fn group(&mut self) {
+        *self = self
+            .iter()
+            .fold(
+                HashMap::new(),
+                |mut list: HashMap<String, TableLsPool>, item| {
+                    list.entry(item.id.clone())
+                        .and_modify(|m| {
+                            if m.last_version >= item.last_version {
+                                return;
+                            }
+                            *m = item.clone();
+                        })
+                        .or_insert(item.clone());
+                    list
+                },
+            )
+            .into_values()
+            .collect();
     }
 }
 
@@ -89,9 +168,7 @@ impl InsertToDb for Vec<TableLsEvent> {
 }
 
 #[allow(clippy::enum_variant_names)]
-#[derive(
-    Clone, Copy, Debug, diesel_derive_enum::DbEnum, diesel::query_builder::QueryId, PartialEq, Eq,
-)]
+#[derive(Clone, Copy, Debug, diesel_derive_enum::DbEnum, PartialEq, Eq)]
 #[ExistingTypePath = "crate::schema::sql_types::EventType"]
 pub enum LsEventType {
     // When new pool created.
